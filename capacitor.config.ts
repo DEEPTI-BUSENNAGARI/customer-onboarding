5c6dbea10617ec86cdc5703fc3dab9ae6132a5b6
DEEPTI BUSENNAGARI <92025596+DEEPTI-BUSENNAGARI@users.noreply.github.com>
2023-09-12 14:06:57 +0530

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,11 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'customer_onboarding',
+  webDir: 'dist/customer_onboarding',
+  bundledWebRuntime: false
+};
+
+export default config;
+
